a3c65be4889c2bb4edadeed6ead012d2ae0e8026
steven huynh <steven.h@live.com>
2024-02-05 13:32:10 -0800

    Fix link name

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -5,8 +5,8 @@ function Footer()
 	return (
 		<div className="footer">
 			<Link to='/'>Home</Link>
-			<Link to='/red'>Blue</Link>
 			<Link to='/blue'>Blue</Link>
+			<Link to='/red'>Red</Link>
 		</div>
 	)
 }
